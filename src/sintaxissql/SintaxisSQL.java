b9f9741d99fe78515e02b2bb8c40cdedf5aeed4a
BraulioRuiz <43392482+BraulioRuiz@users.noreply.github.com>
2019-10-20 14:47:22 -0500

    proyecto

---
+++ src/sintaxissql/SintaxisSQL.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sintaxissql;
+
+/**
+ *
+ * @author ayax9
+ */
+public class SintaxisSQL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
